26a98d92de74260f3bbbcc6372450b499e6f0ba7
gautamkrishnar <r.gautamkrishna@gmail.com>
2017-09-10 12:14:37 +0530

    Initial commit

---
+++ src/prevent-multitab.js
@@ -0,0 +1,13 @@
+
+
+var delayCheckTab = function(delay){
+    console.log(delay);
+
+};
+var inactivityDetectorTab = function () {
+    console.log("indect");
+};
+module.exports = {
+    "delayCheckTab" : delayCheckTab,
+    "inactivityDetectorTab": inactivityDetectorTab
+};
---
+++ tests/test.js
@@ -0,0 +1,10 @@
+/*
+*      Prevent MultiTab test file
+ */
+
+var prevent_multitab = require("../src/prevent-multitab");
+
+// Testing the functions
+
+prevent_multitab.inactivityDetectorTab();
+prevent_multitab.delayCheckTab(1000);
